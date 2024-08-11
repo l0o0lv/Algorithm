8f02d0085325ab4b1e1f588faf9d1624f91fbaf8
dongri <128709695+l0o0lv@users.noreply.github.com>
2024-08-11 16:40:10 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.32 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i = 0 ; i < score.length ; i++){
+            if(queue.size() < k){
+                queue.add(score[i]);
+                answer[i] = queue.peek();
+            }
+            else if(queue.size() == k && queue.peek() <= score[i]){
+                queue.poll();
+                queue.add(score[i]);
+                answer[i] = queue.peek();
+            }
+            else
+                answer[i] = queue.peek();
+        }
+        return answer;
+    }
+}
