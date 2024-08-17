b3694496f958b8d3e4acff770de83e954a5783ef
dongri <128709695+l0o0lv@users.noreply.github.com>
2024-08-17 14:09:47 +0900

    [level 1] Title: 폰켓몬, Time: 30.44 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!list.contains(nums[i]))
+                list.add(nums[i]);
+        }
+        if(list.size() >= nums.length / 2)
+            return nums.length / 2;
+        else
+            return list.size();
+    }
+}
