e47eae99112e37b89140fe8bd0767a752f3e4417
dongri <128709695+l0o0lv@users.noreply.github.com>
2024-10-13 13:47:34 +0900

    [D1] Title: 신문 헤드라인, Time: 106 ms, Memory: 16,080 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        System.out.print(str.toUpperCase());
+	}
+}
