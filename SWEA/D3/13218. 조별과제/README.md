10f91e9439905a5694332eb692a7b5b2b1114114
dongri <128709695+l0o0lv@users.noreply.github.com>
2024-11-03 23:05:31 +0900

    [D3] Title: 조별과제, Time: 131 ms, Memory: 19,412 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/README.md
@@ -0,0 +1,15 @@
+# [D3] 조별과제 - 13218 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXzjvCCq-PwDFASs) 
+
+### 성능 요약
+
+메모리: 19,412 KB, 시간: 131 ms, 코드길이: 478 Bytes
+
+### 제출 일자
+
+2024-11-03 23:05
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        for(int i = 1 ; i <= testCase ; i++){
+                System.out.println("#" + i + " " + Integer.parseInt(br.readLine()) / 3);
+            }
+        }
+    }
