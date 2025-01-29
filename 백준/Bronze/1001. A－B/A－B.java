096db24f2d987dd8d8aa78f0407e3bc931368c36
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-01-30 01:59:43 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.print(a-b);
+    }
+}
