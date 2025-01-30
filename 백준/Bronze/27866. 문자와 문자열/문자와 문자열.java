d30fb1b5a74f7b542b199946784b3289cc22539d
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-01-30 12:37:43 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -5,9 +5,9 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine(); // 문자열 입력
-        int i = Integer.parseInt(br.readLine()); // 인덱스 입력
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
 
-        System.out.println(S.charAt(i - 1)); // i번째 문자 출력 (0-based index)
+        System.out.println(S.charAt(i - 1));
     }
 }
