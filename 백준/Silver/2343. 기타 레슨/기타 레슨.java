dd3163deed0cc873c31e225edc7f91aad9b8c40f
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-02-11 13:08:28 +0900

    Update 기타 레슨.java

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -9,42 +9,44 @@ class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int left = 0;
-        int right = 0;
 
         st = new StringTokenizer(br.readLine());
+        int[] lesson = new int[N];
+        int right = 0;
+        int left = 0;
         
         for(int i = 0 ; i < N ; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            left = Math.max(left,arr[i]);
-            right += arr[i];
+            lesson[i] = Integer.parseInt(st.nextToken());
+            left = Math.max(left,lesson[i]);
+            right += lesson[i];
         }
 
         while(left <= right){
-            int mid = (left + right) / 2;
+            int mid = (right + left) /2;
 
-            int count = 1;
-            int sum = 0;
-
-            for(int i = 0 ; i < N ; i++){
-                if(sum + arr[i] > mid){
-                    count++;
-                    sum = arr[i];
-                } else {
-                    sum += arr[i];
-                }
-            }
-
-            if(count > M){ //블루레이 개수가 많다면
-                left = mid + 1; //블루레이 사이즈가 작은 것, 크기를 늘려준다.
-            } else{ //적거나 같다면
-                right = mid - 1; //블루레이 사이즈가 큰 것, 크기를 줄여준다.
-                //else if(count < M) 을 하면 안됨.
-                //count == M 인 경우에도 해줘야 한다.
-                //더 작은 크기로 가능할지 확인해야 하기 때문
+            int count = BSearch(mid,lesson);
+            
+            if(count > M){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        System.out.println(left);
+        System.out.println(left);   
+    }
+
+    static int BSearch(int mid, int lesson[]){
+        int sum = 0;
+        int count = 1;
+
+        for(int i = 0 ; i < lesson.length ; i++){
+            if(sum + lesson[i] > mid){
+                count++;
+                sum = lesson[i];
+            } else {
+                sum += lesson[i];
+            }
+        }
+        return count;
     }
 }
