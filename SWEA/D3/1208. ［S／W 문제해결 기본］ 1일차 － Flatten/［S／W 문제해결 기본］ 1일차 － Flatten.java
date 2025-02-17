0e2573721e260cd1280c774215b1008a4d422468
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-02-17 13:56:09 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 140 ms, Memory: 20,808 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -2,43 +2,37 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Solution {
+class Solution {
     public static void main(String[] args) throws Exception{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st;
-        int testCase = 10;
 
-        for(int i = 1 ; i <=  testCase ; i++){
-            System.out.print("#" + i + " ");
-
-            int tryCount = Integer.parseInt(br.readLine());
-            List<Integer> list = new ArrayList<>();
+        for(int testCase = 1 ; testCase <= 10 ; testCase++){
+            int maxFlattenNum = Integer.parseInt(br.readLine()); //평탄화 횟수
             st = new StringTokenizer(br.readLine());
-
-            for(int j = 0 ; j < 100 ; j++)
-                list.add(Integer.parseInt(st.nextToken()));
-
-            System.out.println(getdistance(list,tryCount));
             
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for(int index = 0 ; index < 100 ; index++){
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+
+            for(int doFlatten = 0 ; doFlatten < maxFlattenNum ; doFlatten++){
+                int maxIndex = list.indexOf(Collections.max(list));
+                int minIndex = list.indexOf(Collections.min(list));
+                int flattenMaxNum = list.get(maxIndex) - 1;
+                int flattenMinNum = list.get(minIndex) + 1;
+                
+                list.set(maxIndex, flattenMaxNum);
+                list.set(minIndex, flattenMinNum);
+
+                if(flattenMaxNum - flattenMinNum == 0)
+                    break;
+            }
+
+            int answer = Collections.max(list) - Collections.min(list);
+            System.out.println("#" + testCase + " " + answer);
         }
-    }
-
-    public static int getdistance(List<Integer> list, int tryCount){
-        for(int k = 0 ; k < tryCount ; k++){
-            int max = Collections.max(list);
-            int min = Collections.min(list);
-
-            if((max - min) == 0)
-                return 0;
-
-            list.set(list.indexOf(max), max - 1);
-            list.set(list.indexOf(min), min + 1);
-        }
-
-        int max = Collections.max(list);
-        int min = Collections.min(list);
-
-        return max - min;
     }
 }
