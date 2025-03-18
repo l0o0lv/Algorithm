8013df32bd18f4673299ed642c7d81251ee21325
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-03-18 13:52:18 +0900

    Update 암호 만들기.java

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,7 +4,6 @@ import java.io.*;
 class Main {
     static int L, C;
     static String[] alphabet;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
     static void dfs(int index, String text, int jaum, int moum){
@@ -14,8 +13,6 @@ class Main {
         }
 
         for(int i = index ; i < C ; i++){
-            if(!visited[i]){
-                visited[i] = true;
                 String letter = alphabet[i];
                 if(letter.equals("a") || letter.equals("e") || 
                    letter.equals("i") || letter.equals("o") || letter.equals("u")){
@@ -23,8 +20,6 @@ class Main {
                 } else {
                     dfs(i + 1, text + letter, jaum + 1, moum);
                 }
-                visited[i] = false;
-            }
         }
     }
     
@@ -35,7 +30,6 @@ class Main {
         L = Integer.parseInt(st.nextToken()); // 암호 길이
         C = Integer.parseInt(st.nextToken()); // 주어지는 문자 개수
         alphabet = new String[C];
-        visited = new boolean[C];
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < C ; i++){
