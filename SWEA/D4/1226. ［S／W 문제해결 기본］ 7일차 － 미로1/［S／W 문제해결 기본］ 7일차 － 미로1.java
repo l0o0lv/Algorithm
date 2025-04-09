1c08af48a8104a3d4fcb3413a4dfe25f66c8769d
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-04-09 21:37:46 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 77 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,59 +1,65 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 class Solution {
-    static int[][] miro;
-    static boolean[][] visited;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
+    static boolean[][] visited;
+    static int[][] arr;
+    static int startX, startY;
 
-    public static class Node{
-        int x;
-        int y;
+    static class Node{
+        int x, y;
+
         public Node(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for(int tc = 1 ; tc <= 10 ; tc++){
-            int testCase = Integer.parseInt(br.readLine());
-
-            miro = new int[16][16];
-            visited = new boolean[16][16];
-            
-            for(int i = 0 ; i < miro.length ; i++){
-                String text = br.readLine();
-                for(int j = 0 ; j < miro.length ; j++){
-                    miro[i][j] = text.charAt(j) - '0';
-                }
-            }
-            System.out.println("#" + tc + " " + BFS());
-        }
-    }
-
-    static int BFS(){
+    static int bfs(){
         Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(1,1));
-        visited[1][1] = true;
+
+        q.add(new Node(startX, startY));
 
         while(!q.isEmpty()){
             Node now = q.poll();
-            
+
             for(int i = 0 ; i < 4 ; i++){
                 int nextX = now.x + dx[i];
                 int nextY = now.y + dy[i];
 
-                if(miro[nextX][nextY] == 3) return 1;
-                if(miro[nextX][nextY] == 0 && !visited[nextX][nextY]){
-                    q.offer(new Node(nextX,nextY));
-                    visited[nextX][nextY] = true;
-                }
+                if(nextX < 0 || nextX >= 16 ||
+                   nextY < 0 || nextY >= 16 ||
+                  arr[nextX][nextY] == 1) continue;
+                if(visited[nextX][nextY]) continue;
+
+                if(arr[nextX][nextY] == 3) return 1;
+                q.add(new Node(nextX, nextY));
+                visited[nextX][nextY] = true;
+                
             }
         }
         return 0;
+    }
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for(int tc = 1 ; tc <= 10 ; tc++){
+            br.readLine();
+            visited = new boolean[16][16];
+            arr = new int[16][16];
+            for(int i = 0 ; i < 16 ; i++){
+                String text = br.readLine();
+                for(int j = 0 ; j < 16 ; j++){
+                    arr[i][j] = text.charAt(j) - '0';
+                    if(arr[i][j] == 2){
+                        startX = i;
+                        startY = j;
+                    }
+                }
+            }
+            System.out.println("#" + tc + " " + bfs());
+        }
     }
 }
