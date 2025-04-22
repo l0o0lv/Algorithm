f9b6196ad55cfe4a2bde229f2c59e8bb2bae8f09
dongri <128709695+l0o0lv@users.noreply.github.com>
2025-04-22 14:22:25 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 25016 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Main {
-    static int[] arr;
-    static boolean[] visited;
-    static StringBuilder sb;
-    static int N, M;
-    
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[M];
-        visited = new boolean[N];
-
-        dfs(0);
-
-        System.out.print(sb);
-    }
-
-    static void dfs(int depth){
-        if(depth == M){
-            for(int num : arr){
-                sb.append(num).append(" ");
-            }
-            sb.append("\n");
-            return; // return 반드시 필요
-        }
-
-        for(int i = 0 ; i < N ; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+public class Main {
+	static int N,M;
+	static boolean[] visited;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[M];
+		visited = new boolean[N+1];
+		back(0);
+		
+		System.out.println(sb.toString());
+		
+	}
+	private static void back(int depth) {
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=1; i<N+1; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i;
+				back(depth+1);
+				visited[i] = false;
+			}
+		}
+	}
 }
