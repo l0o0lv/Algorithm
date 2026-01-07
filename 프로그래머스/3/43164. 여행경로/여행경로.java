87921bfe8583ae32f1452b4773dc8224e12307ae
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-01-08 00:20:40 +0900

    [level 3] Title: 여행경로, Time: 103.48 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -6,23 +6,23 @@ class Solution {
     
     public String[] solution(String[][] tickets) {
         String[] answer = {};
+        
         allRoutes = new ArrayList<>();
         visited = new boolean[tickets.length];
-        
         dfs("ICN", "ICN", tickets, 0);
         Collections.sort(allRoutes);
         answer = allRoutes.get(0).split(" ");
         return answer;
     }
     
-    static void dfs(String now, String route, String[][] ticket, int count){
+    static void dfs(String from, String route, String[][] ticket, int count){
         if(count == ticket.length){
             allRoutes.add(route);
             return;
         }
         
         for(int i = 0 ; i < ticket.length ; i++){
-            if(!visited[i] && now.equals(ticket[i][0])){
+            if(!visited[i] && from.equals(ticket[i][0])){
                 visited[i] = true;
                 dfs(ticket[i][1], route + " " + ticket[i][1], ticket, count + 1);
                 visited[i] = false;
