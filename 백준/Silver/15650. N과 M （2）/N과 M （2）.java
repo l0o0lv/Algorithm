1d686ca749970f5157831751c2c765549f30f9b2
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-01-11 17:21:27 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static int N,M;
-	static boolean[] visited;
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		arr = new int[M];
-		visited = new boolean[N+1];
-		back(1,0);
-		
-		System.out.println(sb.toString());
-		
-	}
-	private static void back(int start,int depth) {
-		if(depth==M) {
-			for(int i=0; i<M; i++) {
-				sb.append(arr[i]).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-		
-		for(int i=start; i<=N; i++) {
-			arr[depth] = i;
-			back(i+1, depth+1);
-			
-		}
-	}
+class Main {
+    static int n, m;
+    static boolean[] visited;
+    static int[] arr;
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
+        arr = new int[m];
+        
+        dfs(1, 0);
+    }
+
+    static void dfs(int num, int cnt){
+        if(cnt == m){
+            for(int i = 0 ; i < m ; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = num ; i <= n ; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[cnt] = i;
+                dfs(i + 1, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
 }
