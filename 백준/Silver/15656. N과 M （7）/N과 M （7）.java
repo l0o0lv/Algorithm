07a77417cfb770bf2338b28d81e4eee76c52f303
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-01-12 16:04:53 +0900

    [Silver III] Title: N과 M (7), Time: 652 ms, Memory: 163392 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static int n, m;
+    static int[] arr, num;
+    static boolean[] visited;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        num = new int[n];
+        arr = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(num);
+        // visited = new boolean[num[num.length - 1] + 1];
+
+        dfs(0, 0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int index, int cnt) throws Exception{
+        if(cnt == m){
+            for(int i = 0 ; i < m ; i++){
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            arr[cnt] = num[i];
+            dfs(i + 1, cnt + 1);
+        }
+    }
+}
