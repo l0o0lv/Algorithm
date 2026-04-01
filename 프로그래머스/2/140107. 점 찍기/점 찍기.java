40ec109ae7dbe1cc93e829f08ff0e3a7eb3367ed
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-04-01 17:42:55 +0900

    [level 2] Title: 점 찍기, Time: 14.85 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -5,7 +5,10 @@ class Solution {
         long answer = 0;
         
         for(int x = 0 ; x <= d ; x += k){
+            // 피타고라스 정리 사용해서 Y 최대길이 구함
             long maxY = (long)Math.sqrt(((long)d * d - (long)x * x));
+            // Y 좌표는 +k 만큼 계속 증가하는 거니까 k를 나눠준다.
+            // 그리고 각 x 좌표에 대한 y가 0인 조합도 넣어줘야 함. 그래서 + 1
             answer += (maxY / k) + 1;
         }
         return answer;
