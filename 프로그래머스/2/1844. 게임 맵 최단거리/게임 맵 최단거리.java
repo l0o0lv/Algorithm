de3630bbe0932c9f017193f26ef375e4f282dcb7
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-05-08 23:53:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.75 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,22 +2,24 @@ import java.util.*;
 
 class Solution {
     static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-        
+    static int answer;
+    static int dx[] = {1, -1, 0 , 0};
+    static int dy[] = {0, 0, 1, -1};
+    
     static class Node{
         int x, y, dist;
-
-        public Node(int x, int y, int dist){
+        
+        public Node(int x, int y , int dist){
             this.x = x;
             this.y = y;
             this.dist = dist;
         }
     }
-    
     public int solution(int[][] maps) {
+        answer = 0;
         visited = new boolean[maps.length][maps[0].length];
-        int answer = bfs(maps);
+        
+        answer = bfs(maps);
         return answer;
     }
     
@@ -29,7 +31,7 @@ class Solution {
         while(!q.isEmpty()){
             Node now = q.poll();
             
-            if(now.x == maps.length -1 && now.y == maps[0].length - 1){
+            if(now.x == maps.length - 1 && now.y == maps[0].length - 1){
                 return now.dist;
             }
             
@@ -37,14 +39,13 @@ class Solution {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
                 
-                if(nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length) continue;
+                if(nx >= maps.length || nx < 0 || ny >= maps[0].length || ny < 0) continue;
                 if(maps[nx][ny] == 1 && !visited[nx][ny]){
-                    q.offer(new Node(nx, ny, now.dist +1));
                     visited[nx][ny] = true;
+                    q.offer(new Node(nx, ny, now.dist + 1));
                 }
             }
         }
-        
         return -1;
     }
 }
