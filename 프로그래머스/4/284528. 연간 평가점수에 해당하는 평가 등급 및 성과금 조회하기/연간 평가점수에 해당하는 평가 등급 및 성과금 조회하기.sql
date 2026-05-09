dce21ef6696bf5344232acdd0576e5c90bad97ef
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-05-09 09:41:52 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,19 @@
+SELECT
+    e.EMP_NO,
+    e.EMP_NAME,
+    CASE
+        WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+    END as GRADE,
+    CASE
+        WHEN AVG(SCORE) >= 96 THEN e.SAL * 0.2
+        WHEN AVG(SCORE) >= 90 THEN e.SAL * 0.15
+        WHEN AVG(SCORE) >= 80 THEN e.SAL * 0.1
+        ELSE 0
+    END as BONUS
+FROM HR_EMPLOYEES e
+JOIN HR_GRADE g ON e.EMP_NO = g.EMP_NO
+GROUP BY e.EMP_NO
+ORDER BY e.EMP_NO ASC
