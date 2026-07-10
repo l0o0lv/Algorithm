0a8f06c1c9d8bce1a0b404cd718e74caf88882eb
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-07-10 09:08:08 +0900

    [Passed] 도로 보수 로봇 (369ms, 23MB)

--- hsat/도로 보수 로봇/road-repair-robot.java
+++ hsat/도로 보수 로봇/road-repair-robot.java
@@ -2,38 +2,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-        int answer = Integer.MAX_VALUE;
-
-        int[] positions = new int[N];
-        for (int i = 0; i < N; i++) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] positions = new int[n];
+        for (int i = 0; i < n; i++) {
             positions[i] = scanner.nextInt();
         }
 
         int left = 1;
-        int right = positions[N-1];
+        int right = positions[n-1];
+        int answer = Integer.MAX_VALUE;
 
         while(left <= right){
             int mid = (left + right) / 2;
             int cnt = 0;
             int length = 0;
 
-            for(int i = 0 ; i < positions.length ; i++){
-                if(positions[i] > length){
+            for(int i = 0 ; i < n ; i++){
+                if(length < positions[i]){
                     cnt++;
-                    length = positions[i] + mid - 1;
+                    length = (positions[i] + mid) - 1;
                 }
             }
-            if(cnt > K){
+
+            if(cnt > k){
                 left = mid + 1;
             }
-            else if(cnt <= K){
-                right = mid - 1;
+            else if(cnt <= k){
                 answer = Math.min(answer, mid);
+                right = mid - 1;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(answer);   
     }
 }
