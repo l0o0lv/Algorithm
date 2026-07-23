aa35b5a81cbf869520978f2320aeacf617f6be8f
dongri <128709695+l0o0lv@users.noreply.github.com>
2026-07-23 09:53:00 +0900

    [Passed] 자동차 테스트 (1579ms, 69MB)

--- hsat/자동차 테스트/car-test.java
+++ hsat/자동차 테스트/car-test.java
@@ -2,27 +2,32 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
         StringBuilder sb = new StringBuilder();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int q = sc.nextInt();
-        HashMap<Integer, Integer> hm = new HashMap<>();
-
         int[] a = new int[n];
-        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
-        Arrays.sort(a);
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
         
-        int[] queries = new int[q];
-        for (int i = 0; i < q; i++) queries[i] = sc.nextInt();
+        Arrays.sort(a);
 
         for(int i = 0 ; i < n ; i++){
-            hm.put(a[i], (i * ((n - 1) - i)));
+            hm.put(a[i], i + 1);
         }
 
-        for(int i = 0 ; i < q ; i++){
-            int query = queries[i];
-
-            sb.append(hm.getOrDefault(query, 0) + "\n");
+        for (int i = 0; i < q; i++){
+            int query = sc.nextInt();
+            
+            if(!hm.containsKey(query)){
+                sb.append("0\n");
+            }
+            else{
+                int idx = hm.get(query);
+                int answer = (idx - 1) * (n - idx);
+                sb.append(answer + "\n");
+            }
         }
 
         System.out.println(sb.toString());
